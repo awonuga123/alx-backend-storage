b517b385e67f94e49315e2448aa3cec0ac986cd7
awonuga ismail <104797919+awonuga123@users.noreply.github.com>
2023-04-13 11:54:10 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
+""" Tracker calls """
 
-'''
-method that implements an expiring web cache and tracker
-'''
-
-from typing import Callable
-from functools import wraps
 import redis
 import requests
+from typing import Callable
+from functools import wraps
+
+r = redis.Redis()
 
 
-def requests_counter(method: Callable) -> Callable:
-    """ Counts how many times a request has been made
-    """
-    r = redis.Redis()
+def count_calls(method: Callable) -> Callable:
+    """ Decorator to know the number of calls """
 
     @wraps(method)
     def wrapper(url):
-        """ wrapper fxn that counts actual no of requests made"""
+        """ Wrapper decorator """
         r.incr(f"count:{url}")
-        cached_ = r.get(f"cached:{url}")
-        if cached:
-            return cached.decode('utf-8')
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
         html = method(url)
         r.setex(f"cached:{url}", 10, html)
@@ -30,9 +27,9 @@ def requests_counter(method: Callable) -> Callable:
     return wrapper
 
 
-@requests_counter
+@count_calls
 def get_page(url: str) -> str:
-    """ obtains html content for a given site url and returns it.
+    """ Get page
     """
-    resp = requests.get(url)
-    return resp.text
+    req = requests.get(url)
+    return req.text
